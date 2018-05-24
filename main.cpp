43eeac3785ec01ec2da221308c2b80db7bf9e447
Max <silverhammermba@gmail.com>
2018-05-24 18:34:02 -0400

    add non-working audio code

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char** argv)
 	Chip8 chip8;
 	chip8.load_rom(argv[1]);
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
 	{
 		std::cerr << "SDL_Init: " << SDL_GetError() << std::endl;
 		return EXIT_FAILURE;
@@ -48,6 +48,29 @@ int main(int argc, char** argv)
 		{SDL_SCANCODE_A, 0x7}, {SDL_SCANCODE_S, 0x8}, {SDL_SCANCODE_D, 0x9}, {SDL_SCANCODE_F, 0xe},
 		{SDL_SCANCODE_Z, 0xa}, {SDL_SCANCODE_X, 0x0}, {SDL_SCANCODE_C, 0xb}, {SDL_SCANCODE_V, 0xf},
 	};
+
+	uint32_t wav_length; // length of sample
+	uint8_t* wav_buffer; // buffer containing audio data
+	SDL_AudioSpec wav_spec; // metadata for audio
+
+	SDL_LoadWAV populates wav_spec, wave_buffer, wave_length;
+
+	wav_spec.callback = stream_audio; // populates audio buffer
+	wav_spec.userdata = NULL; // pointer passed to callback
+
+	// need to pass these in user data, I think
+	uint8_t* audio_pos = wav_buffer; // audio buffer
+	uint32_t* audio_len = wav_length; // remaining length of audio buffer
+
+	if (SDL_OpenAudio(&wav_spec, NULL) < 0)
+	{
+		std::cerr << "SDL_OpenAudio: " << SDL_GetError() << std::endl;
+		// TODO can just leave it silent instead
+		return EXIT_FAILURE;
+	}
+
+	// start playing
+	SDL_PauseAudio(false);
 
 	while (running)
 	{
@@ -114,9 +137,25 @@ int main(int argc, char** argv)
 		SDL_Delay(1);
 	}
 
+	SDL_CloseAudio();
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return EXIT_SUCCESS;
+}
+
+// called when we need more audio to play
+void stream_audio(void* userdata, uint8_t* stream, int length)
+{
+	if (audio_len == 0) return; // nothing left to play
+
+	// clip to end of audio
+	length = (length > audio_length ? audio_length : length);
+
+	// read length bytes from audio_pos into stream, could instead use SDL_memcpy
+	SDL_MixAudio(stream, audio_pos, length, SDL_MIX_MAXVOLUME);
+
+	audio_pos += length;
+	audio_len -= length;
 }
